ef1d0eb2dbf5249975b82e59b56b86d44b27c6ef
Sanjidul Alom Shifat <121656485+Sanjidul-Alom-Shifat@users.noreply.github.com>
2023-02-14 23:40:57 +0600

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,29 @@
+// Step 1 set event
+const login = document.getElementById("login");
+login.addEventListener("click", function () {
+  // check if working
+  //   console.log("login clicked");
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  //   console.log(email, password);
+  if (!email) {
+    alert("Please provide email");
+    return;
+  } else if (!password) {
+    alert("Please provide password");
+    return;
+  } else {
+    console.log(email);
+    // Check if login info correct
+    if (email !== "programminghero@gmail.com") {
+      alert("Wrong email and enter the correct email");
+      return;
+    } else if (password !== "12345") {
+      alert("Wrong password");
+      return;
+    } else {
+      window.location.href = "/depositewithdraw.html";
+    }
+  }
+  console.log("login success");
+});
